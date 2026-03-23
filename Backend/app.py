6496648e8690fe265d60e22293ef4c1db83598ee
harshita <srivastavaharshita1704@gmail.com>
2026-03-23 22:39:19 +0530

    Backend setup with Flask API and preprocessing

---
+++ Backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    text = data.get("text")
+
+    # Temporary dummy output
+    return jsonify({
+        "text": text,
+        "emotion": "Stress",
+        "sentiment": "Negative",
+        "risk": "High"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
